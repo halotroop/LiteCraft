9b4b3f8411e0abf27809ccb56ab159081b84477f
halotroop2288 <carolinejoybell@gmail.com>
2019-09-16 19:35:18 -0700

    Maven setup

--- src/com/github/halotroop/litecraft/LiteCraftMain.java
+++ src/com/github/halotroop/litecraft/LiteCraftMain.java
@@ -1,9 +1,29 @@
 package com.github.halotroop.litecraft;
 
+import org.lwjgl.Version;
+import org.lwjgl.glfw.*;
+
 public class LiteCraftMain
-{	
-	public static void main(String[] args)
+{
+	private long window;
+
+	public void run()
 	{
 		
+	}
+
+	private void init()
+	{
+		
+	}
+
+	private void loop()
+	{
+		
+	}
+
+	public static void main(String[] args)
+	{
+		new LiteCraftMain().run();
 	}
 }
