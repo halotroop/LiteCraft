1db9772c56166f8b798c5cdd6fe5a18cc0b544b3
halotroop2288 <carolinejoybell@gmail.com>
2019-09-18 15:12:38 -0700

    I changed som stoof

--- src/com/github/halotroop/litecraft/types/gui/MainMenu.java
+++ src/com/github/halotroop/litecraft/types/gui/MainMenu.java
@@ -1,11 +1,21 @@
 package com.github.halotroop.litecraft.types.gui;
 
 import com.bwyap.engine.gui.GUI;
+import com.bwyap.engine.gui.element.TexturedButton;
+import com.bwyap.engine.gui.element.base.Button;
 
 public class MainMenu extends Menu
 {
 	public MainMenu(float width, float height)
 	{
 		super(width, height);
+		Button b = new TexturedButton(20, 20, 100, 100)
+		{
+			@Override
+			public void onMouseClicked(float x, float y, int mouseButton)
+			{
+				// TODO Auto-generated method stub
+			}
+		};
 	}
 }
